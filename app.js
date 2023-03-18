f2c4797ff47b731a852ca6f289232ef40d4d368f
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-03-17 23:59:28 -0500

    add city.

--- app.js
+++ app.js
@@ -156,3 +156,106 @@ let sum5 = arr.reduce(function (a, b) {
 });
 console.log("sum5", sum5);
 let listSum5 = document.getElementById("td5");
+
+// New City //
+
+// Define Store class
+class Store {
+    constructor(location, minimum, maximum, avrgSold) {
+      this.location = location;
+      this.minimum = minimum;
+      this.maximum = maximum;
+      this.avrgSold = avrgSold;
+    }
+  
+    // Returns a random number of cookies sold per hour
+    calcCustomer() {
+      return Math.ceil((Math.random() * (this.maximum - this.minimum + 1) + this.minimum) * this.avrgSold);
+    }
+  
+    // Renders a new row in the table for this store
+    render() {
+      let table = document.querySelector('.city-table');
+  
+      // Create new row element with id
+      let newRow = document.createElement('tr');
+      newRow.setAttribute('id', `tr${table.rows.length}`);
+  
+      // Create new location cell element
+      let locationCell = document.createElement('td');
+      locationCell.textContent = this.location;
+      newRow.appendChild(locationCell);
+  
+      // Calculate and add sales data for each hour
+      let sales = [];
+      for (let i = 0; i < 14; i++) {
+        let cookiesSold = this.calcCustomer();
+        sales.push(cookiesSold);
+        let listItem = document.createElement('td');
+        listItem.textContent = cookiesSold;
+        newRow.appendChild(listItem);
+      }
+  
+      // Append the new row to the table
+      table.appendChild(newRow);
+    }
+  }
+  
+  // Generate footer row for the table
+  function generateFooter() {
+    let table = document.querySelector('.city-table');
+    let tFoot = table.createTFoot();
+    let footerRow = tFoot.insertRow(-1);
+  
+    let locationCell = document.createElement('td');
+    locationCell.textContent = 'Total';
+    footerRow.appendChild(locationCell);
+  
+    let totalSales = [];
+    let numColumns = table.rows[0].cells.length - 1; // exclude the location cell
+    for (let i = 0; i < numColumns; i++) {
+      totalSales.push(0);
+    }
+  
+    for (let i = 1; i < table.rows.length - 1; i++) { // exclude header and footer rows
+      let row = table.rows[i];
+      for (let j = 1; j < row.cells.length; j++) { // exclude location cell
+        let cellValue = parseInt(row.cells[j].textContent);
+        totalSales[j - 1] += cellValue;
+      }
+    }
+  
+    for (let i = 0; i < numColumns; i++) {
+      let cell = document.createElement('td');
+      cell.textContent = totalSales[i];
+      footerRow.appendChild(cell);
+    }
+  }
+  
+  // Call the function to generate the footer initially
+  generateFooter();
+  
+  // Add event listener for form submission
+  let form = document.querySelector('#cookieStandForm');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent default form behavior
+  
+    // Get the user inputted location name
+    let location = event.target.location.value;
+  
+    // Create a new Store object with location data
+    let newStore = new Store(location, 2, 16, 4.6);
+  
+    // Render a new row in the table for the new store
+    newStore.render();
+  
+    // Reset the form input
+    event.target.reset();
+  
+    let table = document.querySelector('.city-table');
+    table.deleteTFoot();
+  
+    // Update the footer after adding a new store
+    generateFooter();
+  });
+  
